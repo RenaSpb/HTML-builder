418f11b91ebe2f0955354fae4e05a12300c688f6
renat <murzinarenata@gmail.com>
2025-01-16 11:10:54 -0800

    01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'text.txt');
+const readStream = fs.createReadStream(filePath, 'utf8');
+
+readStream.on('data', chunk => {
+    console.log(chunk)
+});
+
+readStream.on('error', err => {
+    console.error('Error reading file:', err.message);
+})
